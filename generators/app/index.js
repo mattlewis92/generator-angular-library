26b76048a269ce3f9465e50384f51ecdd8922151
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2016-05-22 21:25:00 +0100

    Pre-determine most answers to prompts

--- generators/app/index.js
+++ generators/app/index.js
@@ -3,6 +3,7 @@
 const yeoman = require('yeoman-generator');
 const chalk = require('chalk');
 const yosay = require('yosay');
+const _ = require('lodash');
 
 module.exports = yeoman.Base.extend({
   prompting: function() {
@@ -13,44 +14,57 @@ module.exports = yeoman.Base.extend({
 
     const required = val => !!val;
 
-    const prompts = [{
-      type: 'input',
-      name: 'githubUsername',
-      message: 'What is the github project organisation or username?',
-      validate: required
-    }, {
-      type: 'input',
-      name: 'githubRepoName',
-      message: 'What is the github repository name?',
-      default: this.appname
-    }, {
-      type: 'input',
-      name: 'npmModuleName',
-      message: 'What is the npm module name?',
-      default: this.appname
-    }, {
-      type: 'input',
-      name: 'moduleGlobal',
-      message: 'What should the module be exported as on the window for users not using module bundlers?',
-      validate: required
-    }, {
-      type: 'input',
-      name: 'projectTitle',
-      message: 'What is the human readable project title?',
-      default: this.appname
-    }, {
-      type: 'input',
-      name: 'projectDescription',
-      message: 'What is the project description?'
-    }, {
-      type: 'input',
-      name: 'authorName',
-      message: 'What is the author name?'
-    }];
+    const githubUsernamePromise = new Promise((resolve) => {
+      this.user.github.username((err, username) => resolve(username));
+    });
 
-    return this.prompt(prompts).then(props => {
+    return githubUsernamePromise.then(githubUsername => {
+      const prompts = [{
+        type: 'input',
+        name: 'githubUsername',
+        message: 'What is the github project organisation or username?',
+        validate: required,
+        default: githubUsername
+      }, {
+        type: 'input',
+        name: 'githubRepoName',
+        message: 'What is the github repository name?',
+        default: this.appname.replace(/ /g, '-')
+      }, {
+        type: 'input',
+        name: 'npmModuleName',
+        message: 'What is the npm module name?',
+        default: this.appname.replace(/ /g, '-')
+      }, {
+        type: 'input',
+        name: 'moduleGlobal',
+        message: 'What should the module be exported as on the window for users not using module bundlers?',
+        validate: required,
+        default(answers) {
+          return _.camelCase(answers.npmModuleName);
+        }
+      }, {
+        type: 'input',
+        name: 'projectTitle',
+        message: 'What is the human readable project title?',
+        default: this.determineAppname()
+      }, {
+        type: 'input',
+        name: 'projectDescription',
+        message: 'What is the project description?'
+      }, {
+        type: 'input',
+        name: 'authorName',
+        message: 'What is the author name?',
+        default: this.user.git.name()
+      }];
+
+      return this.prompt(prompts);
+
+    }).then(props => {
       this.props = props;
     });
+
   },
 
   writing: function() {
