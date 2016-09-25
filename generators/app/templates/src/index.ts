cf4e14adace565fb38c9fea4630ddf2a687fb3b2
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2016-09-25 13:38:55 +0100

    Remove .ts from module filename import

--- generators/app/templates/src/index.ts
+++ generators/app/templates/src/index.ts
@@ -1,4 +1,4 @@
-export * from './<%- ngModuleFilename %>';
+export * from './<%- ngModuleFilename.replace('.ts', '') %>';
 
 // all components that will be codegen'd need to be exported for AOT to work
 export * from './helloWorld.component';
