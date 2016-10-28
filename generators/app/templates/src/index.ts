855d872d5160ef3388d69fc1ba41305218f0aca0
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2016-10-28 16:21:48 +0100

    Remove hack now angular bug is fixed

--- generators/app/templates/src/index.ts
+++ generators/app/templates/src/index.ts
@@ -1,4 +1,1 @@
 export * from './<%- ngModuleFilename.replace('.ts', '') %>';
-
-// all components that will be codegen'd need to be exported for AOT to work
-export * from './helloWorld.component';
