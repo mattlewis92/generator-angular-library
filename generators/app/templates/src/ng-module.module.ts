71104b8226b8793c4df52c0943919bd07c46a038
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2017-07-21 13:05:24 +0100

    Revert "Remove common module import"
    This reverts commit bce7180cac859cbb5b2e5e92d154d4506f381a0f.

--- generators/app/templates/src/ng-module.module.ts
+++ generators/app/templates/src/ng-module.module.ts
@@ -1,8 +1,10 @@
 import { NgModule, ModuleWithProviders } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { HelloWorldComponent } from './hello-world.component';
 
 @NgModule({
   declarations: [HelloWorldComponent],
+  imports: [CommonModule],
   exports: [HelloWorldComponent]
 })
 export class <%- ngModuleName %> {
