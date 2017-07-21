bce7180cac859cbb5b2e5e92d154d4506f381a0f
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2017-07-21 12:32:24 +0100

    Remove common module import

--- generators/app/templates/src/ng-module.module.ts
+++ generators/app/templates/src/ng-module.module.ts
@@ -1,10 +1,8 @@
 import { NgModule, ModuleWithProviders } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { HelloWorldComponent } from './hello-world.component';
 
 @NgModule({
   declarations: [HelloWorldComponent],
-  imports: [CommonModule],
   exports: [HelloWorldComponent]
 })
 export class <%- ngModuleName %> {
