2ef5153e1989bfabceead61a623cb44dc922c3a4
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2017-07-23 19:48:57 +0100

    Use webpack config utils. Closes #65

--- generators/app/templates/webpack.config.ts
+++ generators/app/templates/webpack.config.ts
@@ -2,59 +2,60 @@ import * as webpack from 'webpack';
 import * as path from 'path';
 import * as HtmlWebpackPlugin from 'html-webpack-plugin';
 import * as ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
+import { getIfUtils, removeEmpty } from 'webpack-config-utils';
 
-const IS_PROD: boolean = process.argv.indexOf('-p') > -1;
+export default (environment = 'development') => {
 
-export default {
-  devtool: IS_PROD ? 'source-map' : 'eval',
-  entry: path.join(__dirname, 'demo', 'entry.ts'),
-  output: {
-    filename: IS_PROD ? '[name]-[chunkhash].js' : '[name].js'
-  },
-  module: {
-    rules: [{
-      test: /\.ts$/,
-      loader: 'tslint-loader',
-      exclude: /node_modules/,
-      enforce: 'pre'
-    }, {
-      test: /\.ts$/,
-      loader: 'ts-loader',
-      exclude: /node_modules/,
-      options: {
-        transpileOnly: !IS_PROD
-      }
-    }]
-  },
-  resolve: {
-    extensions: ['.ts', '.js']
-  },
-  devServer: {
-    port: 8000,
-    inline: true,
-    hot: true,
-    historyApiFallback: true,
-    overlay: true
-  },
-  plugins: [
-    ...(IS_PROD ? [
-      new webpack.optimize.ModuleConcatenationPlugin()
-    ] : [
-      new webpack.HotModuleReplacementPlugin(),
-      new ForkTsCheckerWebpackPlugin({
+  const { ifProduction, ifDevelopment } = getIfUtils(environment);
+
+  return {
+    devtool: ifProduction('source-map', 'eval'),
+    entry: path.join(__dirname, 'demo', 'entry.ts'),
+    output: {
+      filename: ifProduction('[name]-[chunkhash].js', '[name].js')
+    },
+    module: {
+      rules: removeEmpty([{
+        test: /\.ts$/,
+        loader: 'tslint-loader',
+        exclude: /node_modules/,
+        enforce: 'pre'
+      }, {
+        test: /\.ts$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/,
+        options: {
+          transpileOnly: environment === 'development'
+        }
+      }])
+    },
+    resolve: {
+      extensions: ['.ts', '.js']
+    },
+    devServer: {
+      port: 8000,
+      inline: true,
+      hot: true,
+      historyApiFallback: true,
+      overlay: true
+    },
+    plugins: removeEmpty([
+      ifProduction(new webpack.optimize.ModuleConcatenationPlugin()),
+      ifDevelopment(new webpack.HotModuleReplacementPlugin()),
+      ifDevelopment(new ForkTsCheckerWebpackPlugin({
         watch: ['./src', './demo'],
         formatter: 'codeframe'
+      })),
+      new webpack.DefinePlugin({
+        ENV: JSON.stringify(environment)
+      }),
+      new webpack.ContextReplacementPlugin(
+        /angular(\\|\/)core(\\|\/)@angular/,
+        path.join(__dirname, 'src')
+      ),
+      new HtmlWebpackPlugin({
+        template: path.join(__dirname, 'demo', 'index.ejs')
       })
-    ]),
-    new webpack.DefinePlugin({
-      ENV: JSON.stringify(IS_PROD ? 'production' : 'development')
-    }),
-    new webpack.ContextReplacementPlugin(
-      /angular(\\|\/)core(\\|\/)@angular/,
-      path.join(__dirname, 'src')
-    ),
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, 'demo', 'index.ejs')
-    })
-  ]
+    ])
+  };
 };
