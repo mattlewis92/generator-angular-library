a6ff8bfcec0211f283ee7df366834048e782bc6e
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2017-07-31 19:24:36 +0100

    Add typings for plugin

--- generators/app/templates/custom-typings.d.ts
+++ generators/app/templates/custom-typings.d.ts
@@ -3,3 +3,4 @@ declare module 'webpack-angular-externals';
 declare module 'webpack-rxjs-externals';
 declare module 'webpack-config-utils';
 declare module 'offline-plugin';
+declare module '@mattlewis92/webpack-karma-die-hard';
