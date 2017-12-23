fd4b8cbb96e1ff3bc08e128e2d0e63378b06549d
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2017-12-23 21:32:37 +0000

    Remove angular and rxjs external packages

--- generators/app/templates/custom-typings.d.ts
+++ generators/app/templates/custom-typings.d.ts
@@ -1,6 +1,4 @@
 declare module 'fork-ts-checker-webpack-plugin';
-declare module 'webpack-angular-externals';
-declare module 'webpack-rxjs-externals';
 declare module 'webpack-config-utils';
 declare module 'offline-plugin';
 declare module '@mattlewis92/webpack-karma-die-hard';
